7dc61c80867b8b72fcb19c20ac05356987302bdb
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-05-11 23:05:40 -0400

    adding router

--- src/components/pages/Page2.js
+++ src/components/pages/Page2.js
@@ -6,7 +6,7 @@ import Account from "../Account";
 const Page2 = props => {
     return (
         <div className="page-two">
-            <h2>Page 2: Can be shown through tabs</h2>
+            <h2>Transactions</h2>
 
 
         </div>
